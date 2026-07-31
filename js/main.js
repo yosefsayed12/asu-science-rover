ecf01bb51209eee4dadde95bb5c14d0ed1cc9f5c
yosefsayed12 <yosefsayed604@gmail.com>
2026-07-31 19:53:24 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const facebook = document.getElementById("facebook");
-
+const btn = document.querySelector("#join-us");
 facebook.addEventListener("click", () => {
     console.log("Clicked!");
     window.open("https://web.facebook.com/scienceseascouts", "_blank");
 });
+
+btn.addEventListener("click", () => {
+    window.location.href = "joinus.html";
+});
+
