29524c8521610b7a8a46ee15442f7414cfdba79e
yosefsayed12 <yosefsayed604@gmail.com>
2026-08-19 21:08:56 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,83 @@
 const facebook = document.getElementById("facebook");
 const btn = document.querySelector("#join-us");
+
+const modal = document.getElementById("comitteeModal")
+const modalTitle = document.getElementById("modalTitle");
+const modalDescription = document.getElementById("modalDescription");
+const modalTasks = document.getElementById("modalTasks");
+const modalSkills = document.getElementById("modalSkills");
+const closingModel = document.getElementById("closeModel");
+
+const moreButtons = document.querySelectorAll(".more-btn");
+const committees = {
+    media: {
+        title: "لجنة الميديا",
+
+        description: "تتولى لجنة الميديا توثيق الانشطة وفعاليات العشيرة واظهارها بصورة مميزة من خلال التصوير والتصميم وتصوير المحتوى",
+
+        tasks: ["تصوير ونوثيق انشطة وفعاليات العشيرة.",
+                "تصميم المنشورات والمحتوى البصري",
+                "كتابة وتجهيز المحتوى الاعلامي ادارة المحتوى الخاص بوسائل التواصل",
+                "ابراز انشطة وانجازات العشيرة"],
+
+        skills: "التصوير * التصميم * كتابة المحتوى * العمل الجماعي * الابداع"
+    },
+
+    hr: {
+        title: "",
+
+        description: "",
+
+        tasks: [],
+
+        skills: ""
+    },
+
+    pr: {
+        title: "",
+
+        description: "",
+
+        tasks: [],
+
+        skills: ""
+    },
+
+    finan: {
+        title: "",
+
+        description: "",
+
+        tasks: [],
+
+        skills: ""
+    }
+};
+
+moreButtons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        const committeeName = button.dataset.committee;
+        const committee = committees[committeeName];
+
+        modalTitle.textContent = committee.title;
+        modalDescription.textContent = committee.description;
+        modalTasks.innerHTML = "";
+
+        committee.tasks.forEach(task => {
+            const li = document.createElement("li");
+            li.textContent = task;
+            modalTasks.appendChild(li);
+        });
+
+        modalSkills.textContent = committee.skills;
+        modal.style.display = "flex";
+    });
+});
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+})
 facebook.addEventListener("click", () => {
     console.log("Clicked!");
     window.open("https://web.facebook.com/scienceseascouts", "_blank");
@@ -8,4 +86,3 @@ facebook.addEventListener("click", () => {
 btn.addEventListener("click", () => {
     window.location.href = "joinus.html";
 });
-
